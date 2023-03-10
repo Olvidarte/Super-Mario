23a5d8c136c45525c0c467c98577f726986a3460
Samantha Chang <olvidarteloo@gmil.com>
2023-03-10 10:59:09 +0800

    Initial Commit

---
+++ Super Mario/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Super Mario
+//
+//  Created by 張雅婷 on 2023/3/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
